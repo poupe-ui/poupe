cc5be34aee3840b0a9fcababdb4bc6afc0cee44d
Alejandro Mery <amery@apptly.co>
2025-01-03 17:49:03 +0000

    feat(theme-builder): tailwind-theme: include shades of palette colors
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/theme-builder/src/tailwind-theme.ts
+++ packages/theme-builder/src/tailwind-theme.ts
@@ -1,8 +1,13 @@
 import {
+  Hct,
+} from './core';
+
+import {
   type MakeCSSThemeOptions,
 
   assembleCSSColors,
 } from './dynamic-color-css';
+
 import {
   type ThemeColors,
 
@@ -12,6 +17,10 @@ import {
 import {
   rgbFromHct,
 } from './tailwind-common';
+
+import {
+  makeShades,
+} from './tailwind-shades';
 
 /**
  *  makeCSSTheme assembles CSS variables to use in M3 dark/light TailwindCSS themes.
@@ -23,8 +32,35 @@ import {
 export function makeCSSTheme<K extends string>(colors: ThemeColors<K>,
   options: Partial<MakeCSSThemeOptions> = {},
 ) {
-  const { dark, light } = makeTheme(colors, options.scheme, options.contrastLevel);
-  return assembleCSSColors(dark, light, {
+  const { dark, light, darkPalette, lightPalette } = makeTheme(colors, options.scheme, options.contrastLevel);
+
+  // shades
+  const darkColors: Record<string, Hct> = {
+    ...dark,
+  };
+
+  const lightColors: Record<string, Hct> = {
+    ...light,
+  };
+
+  const keys = Object.keys(darkPalette) as Array<keyof typeof darkPalette>;
+  for (const key of keys) {
+    const darkShades = makeShades(darkPalette[key]);
+    const lightShades = makeShades(lightPalette[key]);
+
+    const shades = Object.keys(darkShades) as Array<keyof typeof darkShades>;
+    for (const shade of shades) {
+      if (shade !== 'DEFAULT') {
+        darkColors[`${key}-${shade}`] = darkShades[shade];
+        lightColors[`${key}-${shade}`] = lightShades[shade];
+      } else if (!(key in dark)) {
+        darkColors[key] = darkPalette[key];
+        lightColors[key] = lightPalette[key];
+      }
+    };
+  }
+
+  return assembleCSSColors(darkColors, lightColors, {
     stringify: rgbFromHct,
     ...options,
   });
