24f0e7153d8576924573167d27b0fc7efe867c0f
Alejandro Mery <amery@apptly.co>
2025-01-03 20:20:34 +0000

    feat(theme-builder): dynamic-theme: introduce makeThemeKeys() variant of makeTheme()
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/theme-builder/src/dynamic-theme.ts
+++ packages/theme-builder/src/dynamic-theme.ts
@@ -1,4 +1,10 @@
 import {
+  type KebabCase,
+
+  kebabCase,
+} from './utils';
+
+import {
   type Color,
   Hct,
 
@@ -7,8 +13,14 @@ import {
 
 import {
   type StandardDynamicSchemeKey,
+  type CustomDynamicColorKey,
 
+  customDynamicColors,
+  standardDynamicColorKeys,
+  StandardDynamicColorKey,
   standardDynamicSchemes,
+  standardPaletteKeys,
+  StandardPaletteKey,
 } from './dynamic-color-data';
 
 import {
@@ -19,10 +31,26 @@ import {
   makeStandardPaletteFromScheme,
 } from './dynamic-color';
 
+/** flattens ThemeColorOptions or ThemeColors */
+function flattenPartialColorOptions(c?: Color | ColorOptions | Partial<ColorOptions>): Partial<ColorOptions> {
+  if (c === undefined) {
+    return {};
+  } else if (c instanceof Hct || typeof c !== 'object') {
+    return { value: c };
+  } else {
+    return c;
+  }
+}
+
 /** flattens ThemeColors */
 function flattenColorOptions(c: Color | ColorOptions): ColorOptions {
   return c instanceof Hct || typeof c !== 'object' ? { value: c } : c;
 }
+
+/**
+ * ThemeColorOptions defines the colors of the theme but without requiring any particular option
+ */
+export type ThemeColorOptions<K extends string> = { primary: Partial<ColorOptions> } & Record<K, Partial<ColorOptions>>;
 
 /**
  * ThemeColors describes colors used to build the theme.
@@ -33,6 +61,18 @@ export type ThemeColors<K extends string> = { primary: Color | ColorOptions } & 
  * FlatThemeColors defines the colors of the theme
  */
 export type FlatThemeColors<K extends string> = { primary: ColorOptions } & Record<K, ColorOptions>;
+
+/** flattens a ThemeColorOptions */
+export function flattenPartialThemeColors<K extends string>(colors: ThemeColorOptions<K> | ThemeColors<K>) {
+  const out = {} as Record<keyof typeof colors, Partial<ColorOptions>>;
+  const keys = Object.keys(colors) as Array<keyof typeof colors>;
+
+  for (const c of keys) {
+    out[c] = flattenPartialColorOptions(colors[c]);
+  }
+
+  return out;
+}
 
 /** flattens a ThemeColors */
 export function flattenThemeColors<K extends string>(colors: ThemeColors<K>) {
@@ -46,6 +86,50 @@ export function flattenThemeColors<K extends string>(colors: ThemeColors<K>) {
   return out;
 }
 
+/**
+ * makeThemeKeys returns the keys of makeTheme makes without calculating
+ * the values.
+ */
+export function makeThemeKeys<K extends string>(colors: ThemeColorOptions<K> | ThemeColors<K>) {
+  type PaletteKey = StandardPaletteKey | KebabCase<K>;
+  type ColorKey = PaletteKey | StandardDynamicColorKey | CustomDynamicColorKey<KebabCase<K>>;
+
+  const configOptions = {} as Record<PaletteKey, Partial<ColorOptions>>;
+  const paletteKeys: PaletteKey[] = [...standardPaletteKeys];
+  const keys: ColorKey[] = [...standardDynamicColorKeys];
+
+  const $colors = Object.keys(colors) as Array<keyof typeof colors>;
+  for (const name of $colors) {
+    const kebabName = kebabCase(name) as KebabCase<K>;
+
+    // preserve config
+    configOptions[kebabName] = flattenPartialColorOptions(colors[name]);
+
+    if (!(kebabName in keys)) {
+      // custom color
+      paletteKeys.push(kebabName);
+      for (const pattern in customDynamicColors) {
+        keys.push(pattern.replace('{}', kebabName) as CustomDynamicColorKey<KebabCase<K>>);
+      }
+    }
+  }
+
+  // additional standard palette colors
+  for (const name of standardPaletteKeys) {
+    if (!(name in keys)) {
+      keys.push(name);
+    }
+    if (!(name in configOptions)) {
+      configOptions[name] = {};
+    }
+  }
+
+  return {
+    keys,
+    paletteKeys,
+    configOptions,
+  };
+}
 /**
  * @param colors - base colors of the theme.
  * @param scheme - Material color scheme to use.
