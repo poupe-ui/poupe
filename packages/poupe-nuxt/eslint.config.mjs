df16e520dbd4d5ffa0a845a94205bc35624781d4
Alejandro Mery <amery@apptly.co>
2025-01-04 22:21:38 +0000

    feat(build): update @poupe/eslint-config to 0.4.8
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/poupe-nuxt/eslint.config.mjs
+++ packages/poupe-nuxt/eslint.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
-import { forNuxt } from '@poupe/eslint-config/nuxt';
+import { forNuxtModules } from '@poupe/eslint-config/nuxt';
 
 // Run `pnpx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
@@ -15,4 +15,4 @@ export default createConfigForNuxt({
       './playground',
     ],
   },
-}, forNuxt());
+}, forNuxtModules());
