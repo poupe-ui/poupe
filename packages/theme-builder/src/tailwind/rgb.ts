12ceb15a1b568f0e24243eeaf76204f0837c968c
Alejandro Mery <amery@apptly.co>
2025-01-21 08:02:14 +0000

    feat(theme-builder): tailwind: introduce universal rgb() helper
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/theme-builder/src/tailwind/rgb.ts
+++ packages/theme-builder/src/tailwind/rgb.ts
@@ -1,17 +1,20 @@
-// imports
-//
 import {
+  type Color,
   Hct,
 
+  argb,
   argbFromHct,
   splitArgb,
 } from '../core/colors';
 
-// rgbFromArgb for tailwindcss color variables.
+/** @returns RGB representation of the given ARGB number for tailwindcss color variables. */
 export const rgbFromArgb = (argb: number) => {
   const { r, g, b } = splitArgb(argb);
   return `${r} ${g} ${b}`;
 };
 
-// rgbFromHct for tailwindcss color variables.
+/** @returns RGB representation of the given {@link Hct} color for tailwindcss color variables. */
 export const rgbFromHct = (c: Hct) => rgbFromArgb(argbFromHct(c));
+
+/** @returns RGB representation of the given {@link Color} for tailwindcss color variables. */
+export const rgb = (c: Color) => rgbFromArgb(argb(c));
