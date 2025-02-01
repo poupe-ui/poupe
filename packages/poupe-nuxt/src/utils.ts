ce7ee9751213495c6f8982556c57795ebf03ea69
Alejandro Mery <amery@apptly.co>
2025-02-01 23:38:41 +0000

    feat(poupe-nuxt): utils: introduce stringify()
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/poupe-nuxt/src/utils.ts
+++ packages/poupe-nuxt/src/utils.ts
@@ -1,6 +1,12 @@
 import { createResolver } from '@nuxt/kit';
+import stringifyObject from 'stringify-object';
 
 export const createDefaultResolver = () => {
   const { resolve } = createResolver(import.meta.url);
   return resolve;
 };
+
+export const stringify = (input: unknown): string => stringifyObject(input, {
+  indent: '  ',
+  singleQuotes: true,
+});
