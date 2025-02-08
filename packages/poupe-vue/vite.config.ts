104d6962bdff24c43d5e38850f165f1546055f9b
Alejandro Mery <amery@apptly.co>
2025-02-08 00:01:32 +0000

    feat(poupe-vue): introduce config export
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/poupe-vue/vite.config.ts
+++ packages/poupe-vue/vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
       fileName: (_, name) => `${name}.mjs`,
       entry: {
         index: resolve('src/index.ts'),
+        config: resolve('src/config.ts'),
         resolver: resolve('src/resolver.ts'),
       },
     },
