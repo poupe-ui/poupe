1ee9a735b2ee0727b9ebdd592a5abf4a25b05006
Alejandro Mery <amery@apptly.co>
2025-02-10 20:40:16 +0000

    feat(poupe-vue): components: Icon wrapper for IconifyIcon
    Signed-off-by: Alejandro Mery <amery@apptly.co>

---
+++ packages/poupe-vue/src/components/icon.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import {
+  type IconifyIconProps,
+} from '@iconify/vue';
+
+export interface IconProps extends Omit<IconifyIconProps, 'icon'> {
+  icon?: IconifyIconProps['icon']
+}
+</script>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { Icon as IconifyIcon } from '@iconify/vue';
+
+type IconifyIconObject = Exclude<IconifyIconProps['icon'], string>;
+
+const props = defineProps<IconProps>();
+const blankIcon: IconifyIconObject = {
+  body: '',
+};
+
+const icon = computed<IconifyIconProps['icon']>(() => props.icon || blankIcon);
+</script>
+
+<template>
+  <IconifyIcon
+    v-bind="props"
+    :icon="icon"
+  />
+</template>
--- packages/poupe-vue/src/components/index.ts
+++ packages/poupe-vue/src/components/index.ts
@@ -1,5 +1,6 @@
 export { type ButtonProps, default as Button } from './button.vue';
 export { type CardProps, default as Card } from './card.vue';
+export { type IconProps, default as Icon } from './icon.vue';
 export { type PlaceholderProps, default as Placeholder } from './placeholder.vue';
 
 export * from './theme';
