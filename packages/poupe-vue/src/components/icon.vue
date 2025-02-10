b36e21297b17ab24b043392a55a2fd1a30ad3acf
Alejandro Mery <amery@apptly.co>
2025-02-10 20:40:16 +0000

    feat(poupe-vue): components: Icon wrapper for IconifyIcon
    Signed-off-by: Alejandro Mery <amery@apptly.co>

---
+++ packages/poupe-vue/src/components/icon.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import {
+  type IconifyIconProps,
+} from '@iconify/vue';
+
+export interface IconProps extends Omit<IconifyIconProps, 'icon'> {
+  icon?: IconifyIconProps['icon']
+}
+</script>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { Icon as IconifyIcon } from '@iconify/vue';
+import { usePoupeIcons } from '@/composables/use-icons';
+
+const props = defineProps<IconProps>();
+
+const { toIcon } = usePoupeIcons();
+
+const icon = computed<IconifyIconProps['icon']>(() => toIcon(props.icon));
+</script>
+
+<template>
+  <IconifyIcon
+    v-bind="props"
+    :icon="icon"
+  />
+</template>
