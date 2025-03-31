a84cd29b49665e62ef0cc90a242f1e2c25378b7e
Alejandro Mery <amery@apptly.co>
2025-03-31 19:29:39 +0000

    feat(theme-builder): introduce @poupe/theme-builder/server
    with helpers to implement server-side rendering
    
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/theme-builder/src/core.ts
+++ packages/theme-builder/src/core.ts
@@ -1,4 +1,4 @@
-// imports
+// dependencies
 //
 import {
   type CustomColor,
---
+++ packages/theme-builder/src/server/utils.ts
@@ -0,0 +1,39 @@
+import {
+  type HexColor,
+} from '../core';
+
+import {
+  type StandardDynamicSchemeKey,
+
+  standardDynamicSchemes,
+} from '../dynamic-color-data';
+
+const reHexValue = /^#?(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+
+/** @returns if the value is a string suitable for {@link hct} */
+export const isHexValue = (s: string | HexColor): boolean => reHexValue.test(s);
+
+/** @returns if the value is a valid {@link StandardDynamicSchemeKey} */
+export const isThemeSchemeKey = (s?: string): boolean => !!s && s in standardDynamicSchemes;
+
+/** @returns the given string as a valid StandardDynamicSchemeKey, or undefined if it is not a valid key */
+export const asThemeSchemeKey = (key?: string): StandardDynamicSchemeKey | undefined => {
+  if (isThemeSchemeKey(key)) {
+    return key as StandardDynamicSchemeKey;
+  }
+
+  return undefined;
+};
+
+/** Extracts the first parameter from a potentially undefined or array-like parameter, returning a single string or undefined. */
+export const getParam = (param?: string | string[]): (string | undefined) => {
+  if (param === undefined) {
+    return undefined;
+  } else if (!Array.isArray(param)) {
+    return param;
+  } else if (param.length > 0) {
+    return param[0];
+  } else {
+    return undefined;
+  }
+};
