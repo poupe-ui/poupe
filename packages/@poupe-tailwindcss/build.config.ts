55f2d4701c4cd4d02a5fd0f852d62fc29f7d5d61
Alejandro Mery <amery@apptly.co>
2025-05-08 16:53:13 +0000

    feat(poupe-tailwindcss): make unbuild entrypoints explicit
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/@poupe-tailwindcss/build.config.ts
+++ packages/@poupe-tailwindcss/build.config.ts
@@ -1,5 +1,11 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
+  entries: [
+    { input: 'src/index', name: 'index' },
+    { input: 'src/theme/index', name: 'theme' },
+    { input: 'src/utils/index', name: 'utils' },
+  ],
+  declaration: true,
   sourcemap: true,
 });
