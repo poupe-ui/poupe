890fe881542498dd2f492e9cad8053eca309b021
Alejandro Mery <amery@apptly.co>
2025-05-16 16:43:51 +0000

    feat(nuxt): build: remove unneeded externals and enable sourcemap
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/@poupe-nuxt/build.config.ts
+++ packages/@poupe-nuxt/build.config.ts
@@ -1,8 +1,5 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-  externals: [
-    '@material/material-color-utilities',
-    'defu',
-  ],
+  sourcemap: true,
 });
