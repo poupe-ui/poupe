59b732adb77cf71b47b6c3a71e97e9af4e257409
Alejandro Mery <amery@apptly.co>
2025-05-27 19:05:16 +0000

    feat(theme-builder)!: drop `@poupe/theme-builder/utils`
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/@poupe-theme-builder/build.config.ts
+++ packages/@poupe-theme-builder/build.config.ts
@@ -5,7 +5,6 @@ export default defineBuildConfig({
     { input: 'src/index', name: 'index' },
     { input: 'src/core/index', name: 'core' },
     { input: 'src/server/index', name: 'server' },
-    { input: 'src/utils/index', name: 'utils' },
   ],
   declaration: true,
   sourcemap: true,
