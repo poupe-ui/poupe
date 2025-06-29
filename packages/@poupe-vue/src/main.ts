9f8c45e873428eeab05ae12316e7793de8792fdd
Alejandro Mery <amery@apptly.co>
2025-06-29 10:11:02 +0000

    fix(@poupe/vue): add Poupe provider to main app
    Add the Poupe provider to the main Vue app to fix injection warnings.
    Components that use usePoupe() composable now have access to the global
    context provider.
    
    Fixes: Vue warn: injection "Symbol(poupe)" not found
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/@poupe-vue/src/main.ts
+++ packages/@poupe-vue/src/main.ts
@@ -2,9 +2,13 @@ import './assets/css/main.css';
 
 import { createApp } from 'vue';
 import { createHead } from '@unhead/vue/client';
+import { createPoupe, type PoupeOptions } from './composables/use-poupe';
 
 import App from './app.vue';
 
+const poupeOptions: PoupeOptions = {};
+
 createApp(App)
   .use(createHead())
+  .use(createPoupe(poupeOptions))
   .mount('#app');
